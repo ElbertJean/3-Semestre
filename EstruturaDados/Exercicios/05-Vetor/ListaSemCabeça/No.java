b27cd466a8c875bce0145a72b03daf73819cf872
Elbert Jean <elbertjeandossantos@gmail.com>
2024-09-16 18:27:24 -0300

    estudo lista

---
+++ EstruturaDados/Exercicios/05-Vetor/ListaSemCabeça/No.java
@@ -0,0 +1,25 @@
+public class No {
+  
+  private String valor;
+  private No proximo;
+
+  public No(String valor) {
+    this.valor = valor;
+  }
+
+  public String getValor() {
+    return valor;
+  }
+
+  public void setValor(String valor) {
+    this.valor = valor;
+  }
+
+  public No getProximo() {
+    return proximo;
+  }
+
+  public void setProximo(No proximo) {
+    this.proximo = proximo;
+  }
+}
